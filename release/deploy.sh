a190e733bec85c77c05103b7861fedb70f25f022
Richard North <rich.north@gmail.com>
2017-07-09 20:16:55 +0100

    Update codesigning credentials

--- release/deploy.sh
+++ release/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-openssl aes-256-cbc -K $encrypted_4ed9d1624ee2_key -iv $encrypted_4ed9d1624ee2_iv -in release/codesigning.asc.enc -out release/codesigning.asc -d
+openssl aes-256-cbc -K $encrypted_0670d12ce6c3_key -iv $encrypted_0670d12ce6c3_iv -in release/codesigning.asc.enc -out release/codesigning.asc -d
 gpg --fast-import release/codesigning.asc
 
 mvn versions:set -DnewVersion=$TRAVIS_TAG
