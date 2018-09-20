eca519e10f2fc8e93b780c9017aa9b5d96f07795
Richard North <rich.north@gmail.com>
2018-09-20 20:51:39 +0100

    Remove content. This module has been relocated to https://github.com/testcontainers/testcontainers-java

--- release/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-openssl aes-256-cbc -K $encrypted_0670d12ce6c3_key -iv $encrypted_0670d12ce6c3_iv -in release/codesigning.asc.enc -out release/codesigning.asc -d
-gpg --fast-import release/codesigning.asc
-
-mvn versions:set -DnewVersion=$TRAVIS_TAG
-mvn deploy -P sign,build-extras -DskipTests --settings release/settings.xml
